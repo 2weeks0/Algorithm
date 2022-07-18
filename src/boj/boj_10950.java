d2979f9057605b80f9a9c6f3a142f5a29bbd4edc
이주영 <dlendjs93@gmail.com>
2022-07-18 23:20:53 +0900

    0718

---
+++ src/boj/boj_10950.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj_10950 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            bw.append(String.valueOf(sum)).append('\n');
+        }
+        bw.close();
+        br.close();
+    }
+}
