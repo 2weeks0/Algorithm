a8c5aee541de94dadf45e53e4b800dbb110e83a9
이주영 <*^^*>
2022-08-04 19:18:44 +0900

    0803

---
+++ src/boj/boj_1032.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		String[] strArray = new String[n];
+		for (int i = 0; i < n; i++) {
+			strArray[i] = br.readLine();
+		}
+
+		StringBuilder sb = new StringBuilder();
+		outer: for (int j = 0; j < strArray[0].length(); j++) {
+			char c = strArray[0].charAt(j);
+			for (int i = 1; i < n; i++) {
+				if (c != strArray[i].charAt(j)) {
+					sb.append('?');
+					continue outer;
+				}
+			}
+			sb.append(c);
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}
